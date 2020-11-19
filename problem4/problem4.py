e01c19f78a889c1d655a786a8f2b7ba1d01100cb
Victor Capone <victor.capone@brainweb.com.br>
2020-11-19 19:18:27 -0300

    Adding a better solution to problem 4

--- problem4/problem4.py
+++ problem4/problem4.py
@@ -9,8 +9,26 @@ def largest_palindrome_product(start, end):
     return greatest
 
 
-def is_palindrome(string):
-    return string == string[::-1]
+#  Generate all 6 digits palindromes and check if they are a product of 2 3-digit numbers
+def largest_palindrome_product_2(digits):
+    start = 999
+    for i in range(0, 899):
+        s = str(start-i)
+        palindrome = "{}{}".format(s, s[::-1])
+        if is_product_3_digits(int(palindrome)):
+            return palindrome
+
+
+def is_product_3_digits(num):
+    for i in range(100, 999):
+        if num % i == 0 and num/i <= 999:
+            return True
+    return False
+
+
+def is_palindrome(str):
+    return str == str[::-1]
 
 
 print(largest_palindrome_product(100, 1000))
+print(largest_palindrome_product_2(3))
