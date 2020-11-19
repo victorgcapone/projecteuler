3c40df6a6ddf914206868150dd895f70e334a3d2
Victor Capone <victor.capone@brainweb.com.br>
2020-11-19 19:29:23 -0300

    Solving problem 5

---
+++ problem5/problem5.py
@@ -0,0 +1,6 @@
+#  For non-prime factors (like 4) we must multiply once more by the prime factor (in this case 2)
+#  So for the number 4, 8, 16 and 9 we multiply by 2, 2, 2 and 3 again
+#  15 and 18 in this case are already covered by 3 * 5 and 3 * 3 * 2
+result = 2*3*2*5*7*2*3*11*13*2*17*19
+
+print(result)
